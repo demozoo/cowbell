589e7996d77e380f37fc33613e6b16d31097d83f
Matt Westcott <matt@west.co.tt>
2024-01-11 01:39:53 +0000

    Upgrade Ubuntu to jammy64 and node.js to v20

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt-get install -y pasmo make
 
 # install Node.js + coffeescript
 # as per instructions on https://github.com/nodesource/distributions
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
 npm install -g coffeescript@1.12.7
 
