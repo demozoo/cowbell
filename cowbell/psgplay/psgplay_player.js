8ff4f5c6edd48e724b0a1b23a8753ffa9e5ae71d
Matt Westcott <matt@west.co.tt>
2024-01-21 04:13:16 +0000

    Bundle libpsgplay.js in psgplay.min.js instead of lazy-loading (#35)
    Fixes #29
    
    `onRuntimeInitialized` only fires once across all emscripten modules, so we can't use it to detect when libpsgplay is loaded if that mechanism has already been spent on libopenmpt. However, we don't really need that mechanism - we only care about lazy-loading the wasm file, not libpsgplay.js (which is only 26kb), and current emscripten provides the MODULARIZE option for doing that.
    
    As a bonus, we no longer need the pathToLibPSGPlay opton.

--- cowbell/psgplay/psgplay_player.js
+++ cowbell/psgplay/psgplay_player.js
@@ -1,6 +1,8 @@
 /* SNDH player (Atari ST) using https://github.com/frno7/psgplay */
 
 (function() {
+	var LibPSGPlay = null;
+
 	function PSGPlayGenerator(url, audioCtx, playerOpts, trackOpts) {
 		var psgplayPtr, bufferPtr;
 		var maxFramesPerChunk = 4096;
@@ -12,85 +14,72 @@
 		function initSNDH(data) {
 			var byteArray = new Int8Array(data);
 			var track = trackOpts.track;
-			bufferPtr = libpsgplay._malloc(maxFramesPerChunk*4); 
+			bufferPtr = LibPSGPlay._malloc(maxFramesPerChunk*4);
 
 			self.duration=0;
 			decrunchedBytes=0;
-			var longPtr = libpsgplay._malloc(4); 
-			if(Module.ccall('ice_identify',
+			var longPtr = LibPSGPlay._malloc(4);
+			if(LibPSGPlay.ccall('ice_identify',
 			  'number',
 			  ['array','number'],
 			  [byteArray, byteArray.byteLength])){ // arguments
-				var s = Module.ccall('ice_decrunched_size','number',['array','number'],[byteArray, byteArray.byteLength]);
-				decrunchedBytes = libpsgplay._malloc(s); 
+				var s = LibPSGPlay.ccall('ice_decrunched_size','number',['array','number'],[byteArray, byteArray.byteLength]);
+				decrunchedBytes = LibPSGPlay._malloc(s);
 
-				if (Module.ccall('ice_decrunch','number',['number','array','number'],[decrunchedBytes, byteArray, byteArray.byteLength]) == -1) {
+				if (LibPSGPlay.ccall('ice_decrunch','number',['number','array','number'],[decrunchedBytes, byteArray, byteArray.byteLength]) == -1) {
 					console.log("ICE decrunch failed\n");
 					return;
 				}
 				if(!trackOpts.track){
-					if(libpsgplay._sndh_tag_default_subtune(longPtr,decrunchedBytes,s)){
-						trackOpts.track=libpsgplay.HEAP32[longPtr/4];
+					if(LibPSGPlay._sndh_tag_default_subtune(longPtr,decrunchedBytes,s)){
+						trackOpts.track=LibPSGPlay.HEAP32[longPtr/4];
 					}else{
 						trackOpts.track=1;
 					}
 				}
-				if(libpsgplay._sndh_tag_subtune_time(longPtr,trackOpts.track,decrunchedBytes,s)){
-					self.duration=libpsgplay.HEAPF32.subarray(longPtr/4 , longPtr/4 + 1)[0];
+				if(LibPSGPlay._sndh_tag_subtune_time(longPtr,trackOpts.track,decrunchedBytes,s)){
+					self.duration=LibPSGPlay.HEAPF32.subarray(longPtr/4 , longPtr/4 + 1)[0];
 				}
-				psgplayPtr = libpsgplay._psgplay_init(decrunchedBytes, s, trackOpts.track, audioCtx.sampleRate);
+				psgplayPtr = LibPSGPlay._psgplay_init(decrunchedBytes, s, trackOpts.track, audioCtx.sampleRate);
 			} else{
 				if(!trackOpts.track){
-					if(Module.ccall('sndh_tag_default_subtune','boolean',['number','array','number'],[longPtr,byteArray, byteArray.byteLength])){
-						trackOpts.track=libpsgplay.HEAP32[longPtr/4];
+					if(LibPSGPlay.ccall('sndh_tag_default_subtune','boolean',['number','array','number'],[longPtr,byteArray, byteArray.byteLength])){
+						trackOpts.track=LibPSGPlay.HEAP32[longPtr/4];
 					}else{
 						trackOpts.track=1;
 					}
 				}
-				if(Module.ccall('sndh_tag_subtune_time','boolean',['number','number','array','number'],[longPtr,trackOpts.track,byteArray, byteArray.byteLength])){
-					self.duration=libpsgplay.HEAPF32.subarray(longPtr/4 , longPtr/4 + 1)[0];
+				if(LibPSGPlay.ccall('sndh_tag_subtune_time','boolean',['number','number','array','number'],[longPtr,trackOpts.track,byteArray, byteArray.byteLength])){
+					self.duration=LibPSGPlay.HEAPF32.subarray(longPtr/4 , longPtr/4 + 1)[0];
 				}
-				psgplayPtr = Module.ccall('psgplay_init','number',['array','number','number','number'],[byteArray, byteArray.byteLength, trackOpts.track, audioCtx.sampleRate]);
+				psgplayPtr = LibPSGPlay.ccall('psgplay_init','number',['array','number','number','number'],[byteArray, byteArray.byteLength, trackOpts.track, audioCtx.sampleRate]);
 			}
 			if (longPtr) {
-				libpsgplay._free(longPtr);
+				LibPSGPlay._free(longPtr);
 			} 
 		}
 
 		function ensureLibPSGPlay(onReady) {
-			if (!playerOpts.pathToLibPSGPlay) {
-				throw "pathToLibPSGPlay not specified";
-			}
-
-			if (window.libpsgplay && window.libpsgplay._psgplay_init) {
-				/* libopenmpt already loaded */
+			if (LibPSGPlay) {
+				/* libpsgplay already loaded */
 				onReady();
 			} else {
-				/* load libopenmpt via <script> tag injection */
-				var head = document.getElementsByTagName("head")[0];
-				var script = document.createElement("script");
-				script.src = playerOpts.pathToLibPSGPlay;
-
-				window.libpsgplay = {
-					onRuntimeInitialized: function() {
-						onReady();
-						head.removeChild(script);
-					}
-				};
-
-				head.appendChild(script);
+				window.libpsgplay().then((result) => {
+					LibPSGPlay = result;
+					onReady();
+				});
 			}
 		}
 
 		this.cleanup = function() {
 			if (psgplayPtr) {
-				libpsgplay._psgplay_free(psgplayPtr);
+				LibPSGPlay._psgplay_free(psgplayPtr);
 			}
 			if (bufferPtr) {
-				libpsgplay._free(bufferPtr);
+				LibPSGPlay._free(bufferPtr);
 			} 
 			if (decrunchedBytes) {
-				libpsgplay._free(decrunchedBytes);
+				LibPSGPlay._free(decrunchedBytes);
 			} 
 		};
 
@@ -127,8 +116,8 @@
 			var ended = false;
 			while (framesToRender > 0) {
 				var framesPerChunk = Math.min(framesToRender, maxFramesPerChunk);
-				var actualFramesPerChunk=Module.ccall('psgplay_read_stereo','number',['number','number','number'],[psgplayPtr, bufferPtr, framesPerChunk]);
-				var rawAudio = libpsgplay.HEAP16.subarray(bufferPtr/2 , bufferPtr/2 + actualFramesPerChunk*2); 
+				var actualFramesPerChunk=LibPSGPlay.ccall('psgplay_read_stereo','number',['number','number','number'],[psgplayPtr, bufferPtr, framesPerChunk]);
+				var rawAudio = LibPSGPlay.HEAP16.subarray(bufferPtr/2 , bufferPtr/2 + actualFramesPerChunk*2);
 				for (var i = 0; i < actualFramesPerChunk; ++i) {
 					outputL[framesRendered + i] = (rawAudio[i*2])/0x8000;
 					outputR[framesRendered + i] = (rawAudio[i*2+1])/0x8000;
